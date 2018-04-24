9cb6241c75825e70f8fd11d21016f46b54c5242b
elia <elias.tarasov@gmail.com>
2018-04-25 01:54:52 +0300

    added astyle PX4 format

--- src/Node.cpp
+++ src/Node.cpp
@@ -2,63 +2,71 @@
 #include <geometry_msgs/Vector3Stamped.h>
 #include <assert.h>
 
-namespace eskf {
+namespace eskf
+{
 
-  Node::Node(const ros::NodeHandle &nh, const ros::NodeHandle &pnh) : nh_(pnh), init_(false) {
-    //  subscribe
-    ROS_INFO("Subscribing to ~imu.");
-    subImu_ = nh_.subscribe<sensor_msgs::Imu>("imu", 1000, &Node::inputCallback, this, ros::TransportHints().tcpNoDelay(true));
-    ROS_INFO("Subscribing to ~pose.");
-    subPOSE_ = nh_.subscribe("pose", 1, &Node::measurementCallback, this);
-    
-    pubRPY_ = nh_.advertise<geometry_msgs::Vector3Stamped>("rpy", 1);
-  }
+Node::Node(const ros::NodeHandle &nh, const ros::NodeHandle &pnh) : nh_(pnh), init_(false)
+{
+	//  subscribe
+	ROS_INFO("Subscribing to ~imu.");
+	subImu_ = nh_.subscribe<sensor_msgs::Imu>("imu", 1000, &Node::inputCallback, this,
+			ros::TransportHints().tcpNoDelay(true));
+	ROS_INFO("Subscribing to ~pose.");
+	subPOSE_ = nh_.subscribe("pose", 1, &Node::measurementCallback, this);
 
-  void Node::inputCallback(const sensor_msgs::ImuConstPtr& imuMsg) {
-    vec3 wm;        //  measured angular rate
-    vec3 am;        //  measured linear acceleration
+	pubRPY_ = nh_.advertise<geometry_msgs::Vector3Stamped>("rpy", 1);
+}
 
-    wm = vec3(imuMsg->angular_velocity.x, imuMsg->angular_velocity.y, imuMsg->angular_velocity.z);
-    am = vec3(imuMsg->linear_acceleration.x, imuMsg->linear_acceleration.y, imuMsg->linear_acceleration.z);
+void Node::inputCallback(const sensor_msgs::ImuConstPtr &imuMsg)
+{
+	vec3 wm;        //  measured angular rate
+	vec3 am;        //  measured linear acceleration
 
-    if (prevStampIMU_.sec != 0) {
+	wm = vec3(imuMsg->angular_velocity.x, imuMsg->angular_velocity.y, imuMsg->angular_velocity.z);
+	am = vec3(imuMsg->linear_acceleration.x, imuMsg->linear_acceleration.y, imuMsg->linear_acceleration.z);
 
-      const double delta = (imuMsg->header.stamp - prevStampIMU_).toSec();
-      
-      if (!init_) {
-        init_ = true;
-        ROS_INFO("Initialized ESKF");
-      }
-      
-      //  run kalman filter
-      eskf_.predict(wm, am, static_cast<uint64_t>(imuMsg->header.stamp.toSec()*1e6f), delta);
-      
-      const eskf::ESKF::quat n2b = eskf_.getQuat();
-      const vec3 orientation = eskf_.getRPY(n2b.matrix());
-      
-      sensor_msgs::Imu imu = *imuMsg;
-      imu.header.seq = 0;
-            
-      // roll-pitch-yaw (rad)
-      geometry_msgs::Vector3Stamped rpy;
-      rpy.header = imu.header;
-      rpy.vector.x = orientation[0];
-      rpy.vector.y = orientation[1];
-      rpy.vector.z = orientation[2];
-      // publish our topics
-      pubRPY_.publish(rpy);
-    }
-    prevStampIMU_ = imuMsg->header.stamp;
-  }
-  
-  void Node::measurementCallback(const geometry_msgs::PoseStampedConstPtr& poseMsg) {
-    if(prevStampPOSE_.sec != 0) {
-      const double delta = (poseMsg->header.stamp - prevStampPOSE_).toSec();
-      // get measurements
-      quat z_q = quat(poseMsg->pose.orientation.w, poseMsg->pose.orientation.x, poseMsg->pose.orientation.y, poseMsg->pose.orientation.z);
-      vec3 z_p = vec3(poseMsg->pose.position.x, poseMsg->pose.position.y, poseMsg->pose.position.z);
-      eskf_.update(z_q, z_p, static_cast<uint64_t>(poseMsg->header.stamp.toSec()*1e6f), delta);
-    }
-    prevStampPOSE_ = poseMsg->header.stamp;
-  }
+	if (prevStampIMU_.sec != 0) {
+
+		const double delta = (imuMsg->header.stamp - prevStampIMU_).toSec();
+
+		if (!init_) {
+			init_ = true;
+			ROS_INFO("Initialized ESKF");
+		}
+
+		//  run kalman filter
+		eskf_.predict(wm, am, static_cast<uint64_t>(imuMsg->header.stamp.toSec() * 1e6f), delta);
+
+		const eskf::ESKF::quat n2b = eskf_.getQuat();
+		const vec3 orientation = eskf_.getRPY(n2b.matrix());
+
+		sensor_msgs::Imu imu = *imuMsg;
+		imu.header.seq = 0;
+
+		// roll-pitch-yaw (rad)
+		geometry_msgs::Vector3Stamped rpy;
+		rpy.header = imu.header;
+		rpy.vector.x = orientation[0];
+		rpy.vector.y = orientation[1];
+		rpy.vector.z = orientation[2];
+		// publish our topics
+		pubRPY_.publish(rpy);
+	}
+
+	prevStampIMU_ = imuMsg->header.stamp;
+}
+
+void Node::measurementCallback(const geometry_msgs::PoseStampedConstPtr &poseMsg)
+{
+	if (prevStampPOSE_.sec != 0) {
+		const double delta = (poseMsg->header.stamp - prevStampPOSE_).toSec();
+		// get measurements
+		quat z_q = quat(poseMsg->pose.orientation.w, poseMsg->pose.orientation.x, poseMsg->pose.orientation.y,
+				poseMsg->pose.orientation.z);
+		vec3 z_p = vec3(poseMsg->pose.position.x, poseMsg->pose.position.y, poseMsg->pose.position.z);
+		eskf_.update(z_q, z_p, static_cast<uint64_t>(poseMsg->header.stamp.toSec() * 1e6f), delta);
+	}
+
+	prevStampPOSE_ = poseMsg->header.stamp;
+}
 }
